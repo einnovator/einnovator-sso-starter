7ae752f9971e3db1093e769e40604c172f83b13a
Jorge Simao <jsimao71@gmail.com>
2019-06-17 23:21:44 +0100

    added doc upload scripts

---
+++ upload-javadoc.bat
@@ -0,0 +1,6 @@
+kubectl exec einnovator-cdn-86d894cc6-lzlxc -- mkdir -p /usr/share/nginx/html/docs/einnovator-sso-starter/latest
+kubectl cp  target/apidocs einnovator-cdn-86d894cc6-lzlxc:/usr/share/nginx/html/docs/einnovator-sso-starter/latest
+
+
+
+
